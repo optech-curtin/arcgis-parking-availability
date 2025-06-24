950635aede88183acde268622b1604d9b10a56ba
Finn Morris <finn.morris@curtin.edu.au>
2025-06-25 07:59:51 +0800

    addede dployment fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@arcgis/core'],
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
