36036ec507f8ab665ec30ab5ee50c78e44a0e95c
Finn Morris <finn.morris@curtin.edu.au>
2025-06-25 08:03:52 +0800

    added base path and asset path

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   transpilePackages: ['@arcgis/core'],
   output: 'export',
   trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/arcgis-parking-availability' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/arcgis-parking-availability' : '',
   images: {
     unoptimized: true
   }
